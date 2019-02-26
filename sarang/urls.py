234a61983184b4e308a5218792667a4ece024017
RejulS <s.rejul10@gmail.com>
2019-02-27 01:55:46 +0530

    Static and Initial Template Added

--- sarang/urls.py
+++ sarang/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from score import views
+from django.views.generic import TemplateView
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
