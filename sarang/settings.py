0b8c053c8330d501fb842179076d06ac4fc7ce8c
RejulS <s.rejul10@gmail.com>
2019-03-01 15:59:06 +0530

    results fixed

--- sarang/settings.py
+++ sarang/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sarang19.herokuapp.com','127.0.0.1:8000']
+ALLOWED_HOSTS = ['sarang19.herokuapp.com','127.0.0.1']
 
 
 # Application definition
--- score/views.py
+++ score/views.py
@@ -4,12 +4,21 @@ from .models import Match
 
 # Create your views here.
 def Match_list(request):
-    match=Match.objects.order_by('-id')[:2][::-1]
-    cm=match[0]
-    nm=match[1]
+    try:
+        match=Match.objects.order_by('-id')[:2][::-1]
+        cm=match[0]
+        nm=match[1]
+    except:
+        match = None
+        cm=''
+        nm=''
     return render(request, 'home.html', {'match':match,'cm':cm,'nm':nm})
 
 def Results_list(request):
-    match=Match.objects.order_by('-id')[2:]
-    curr=match[0]
+    try:
+        match=Match.objects.order_by('-id')[2:]
+        curr=match[0]
+    except:
+        match = None
+        curr = ''
     return render(request, 'results.html', {'match':match,'curr':curr})
