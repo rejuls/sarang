b263191dca368bb8f71d3166a2b34a3599a0e8fb
RejulS <s.rejul10@gmail.com>
2019-03-01 13:57:37 +0530

    changes

--- sarang/settings.py
+++ sarang/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sarang19.herokuapp.com']
 
 
 # Application definition
